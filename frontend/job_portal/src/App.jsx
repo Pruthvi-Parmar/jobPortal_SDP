d0ec9f7bcbd41e76fbd9e78a9069ef3b97cbcaf8
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-01-08 00:07:02 +0530

    frontend module initilized

---
+++ frontend/job_portal/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Job Portal</h1>
+    </>
+  )
+}
+
+export default App
