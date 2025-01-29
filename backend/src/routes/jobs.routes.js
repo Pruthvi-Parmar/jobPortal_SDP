6e98dcd872d14adefecb784624e4ea907d512217
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-01-29 20:46:30 +0530

    roleauth middleware added

--- backend/src/routes/jobs.routes.js
+++ backend/src/routes/jobs.routes.js
@@ -2,11 +2,14 @@ import { Router } from "express";
 import {deleteJob, getJobs, getJobsPostedByRecruiter, postJobs, updateJob,} from "../controllers/jobs.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../middlewares/role.middleware.js";
+
 
 const router = Router()
 
 router.route("/post-job").post(verifyJWT,upload.fields([{ name: 'coverImage', maxCount: 1 }]),postJobs)
-router.route("/get-job").post(getJobs)
+router.route("/get-job").post(getJobs)//router.route("/get-job").post(verifyJWT,authorizeRoles(["jobseeker"]),getJobs)
+//in above route please send accesstoken in fetch header
 router.route("/update-job").post(updateJob)
 router.route("/delete-job").post(deleteJob)
 router.route("/get-posted-job").post(verifyJWT,getJobsPostedByRecruiter)
