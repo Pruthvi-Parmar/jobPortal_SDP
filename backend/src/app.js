674234f012c9b61b3da6d8d8aac5bf1f54a764d7
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-03-27 18:46:30 +0530

    minor changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 app.use(cors({
     origin: [
       "http://localhost:5173",
-      "https://yourfrontend.com"
+      "https://jobconnect-three.vercel.app"
   ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
